1b1e023bccdd2c8acbbff9b227dde6f685fdf9de
Felipe Chernicharo <string7dev@gmail.com>
2021-03-03 00:52:56 -0300

    forçando renderizações desnecessárias

--- src/components/Postlist.tsx
+++ src/components/Postlist.tsx
@@ -1,24 +1,32 @@
 import React, { useEffect, useState } from "react";
-import styles from "../styles/Home.module.css";
 import PostItem, { Post } from "./PostItem";
+
+import styles from "../styles/Home.module.css";
 
 const Postlist = () => {
   const [posts, setPosts] = useState<Post[]>([]);
+  const [newPost, setNewPost] = useState("");
 
   useEffect(() => {
     //
     fetch("https://jsonplaceholder.typicode.com/posts").then((response) => {
       response.json().then((data: Post[]) => setPosts(data));
     });
-    //
   }, []);
 
   return (
-    <div>
-      {posts.map((post) => (
-        <PostItem post={post} key={post.id} />
-      ))}
-    </div>
+    <>
+      <input
+        type="text"
+        onChange={(e) => setNewPost(e.target.value)} // gera uma nova renderização a cada tecla digitada
+        value={newPost}
+      />
+      <div>
+        {posts.map((post) => (
+          <PostItem post={post} key={post.id} />
+        ))}
+      </div>
+    </>
   );
 };
 
